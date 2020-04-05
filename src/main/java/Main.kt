dfc60568438913a590368a8390a010d0eded3108
ChrisBenua <kpbenua@edu.hse.ru>
2020-04-05 18:20:16 +0300

    README + implementation

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,6 +1,9 @@
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.util.*
+import kotlin.math.acos
+import kotlin.math.cos
+import kotlin.math.sin
 
 
 data class Point(val latitude: Float, val longitude: Float)
@@ -9,8 +12,9 @@ data class Person(val id: UUID, val finishPoint: Point)
 
 fun main() {
     val (passengers, drivers) = readPoints()
+    val center = Point(59.981597f,30.214552f)
     for (passenger in passengers) {
-        val suggestedDrivers = suggestDrivers(passenger, drivers)
+        val suggestedDrivers = suggestDrivers(passenger, drivers, center)
         println("Passenger point: ${passenger.finishPoint.latitude}, ${passenger.finishPoint.longitude}")
         for (driver in suggestedDrivers) {
             println("  ${driver.finishPoint.latitude}, ${driver.finishPoint.longitude}")
@@ -18,8 +22,18 @@ fun main() {
     }
 }
 
-fun suggestDrivers(passenger: Person, drivers: Collection<Person>): Collection<Person> {
-    TODO("Implement me")
+fun dist(from: Point, to: Point): Float {
+    return acos(sin(from.latitude) * sin(to.latitude)
+            + cos(from.latitude) * cos(to.latitude) * cos(from.longitude - to.longitude)) * 6371.21f;
+}
+
+fun quality(passenger: Person, driver: Person, center: Point): Float {
+    return (dist(center, driver.finishPoint) /
+            (dist(center, passenger.finishPoint) + dist(passenger.finishPoint, driver.finishPoint)));
+}
+
+fun suggestDrivers(passenger: Person, drivers: Collection<Person>, center: Point): Collection<Person> {
+    return drivers.sortedBy { quality(it, passenger, center) }.reversed();
 }
 
 private fun readPoints(): Participants {
@@ -34,3 +48,5 @@ private fun asPoint(it: String): Point {
     val (lat, lon) = it.split(", ")
     return Point(lat.toFloat(), lon.toFloat())
 }
+
+
